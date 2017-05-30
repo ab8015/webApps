28a74eedcb316ffe77d77b3cac72ec5cacc00828
Alexander Khabarov <ak10315@point38.doc.ic.ac.uk>
2017-05-30 12:35:20 +0100

    index.js: refactor

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+io.on('connection', socket => 
+  socket.on('chat message', msg => io.emit('chat message', msg)));
+http.listen(port, () => console.log('listening on *:' + port));
